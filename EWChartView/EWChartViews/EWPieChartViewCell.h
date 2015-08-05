0b96d0828935a84d86913259aa4edb3d3b4ee32c
wansy <wansy23@163.com>
2015-08-05 12:18:13 +0800

    add title in piechart

---
+++ EWChartView/EWChartViews/EWPieChartViewCell.h
@@ -0,0 +1,13 @@
+//
+//  EWPieChartViewCell.h
+//  EWChartView
+//
+//  Created by wansy on 15/8/5.
+//  Copyright (c) 2015年 wansy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EWPieChartViewCell : NSObject
+
+@end
---
+++ EWChartView/EWChartViews/EWPieChartViewCell.m
@@ -0,0 +1,13 @@
+//
+//  EWPieChartViewCell.m
+//  EWChartView
+//
+//  Created by wansy on 15/8/5.
+//  Copyright (c) 2015年 wansy. All rights reserved.
+//
+
+#import "EWPieChartViewCell.h"
+
+@implementation EWPieChartViewCell
+
+@end
